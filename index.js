4a5e0834363547870b7359f8854b773dc523d339
souhirfar <souhir_farjallah@live.fr>
2023-12-14 10:13:59 +0100

    fgf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "souhir";
-const city = "paris";
-const language = "JS";
+require('dotenv').config()
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
 const message = `I am ${name}, wilder in ${city}, and I love ${language}.`;
 console.log(message);
