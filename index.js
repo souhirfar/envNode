a5c385d9ba6307d914284eadfc73ef105165fd83
souhirfar <souhir_farjallah@live.fr>
2023-12-14 10:25:40 +0100

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
-const name = process.env.MY_NAME;
-const city = process.env.MY_CITY;
-const language = process.env.MY_LANGUAGE;
-const message = `I am ${name}, wilder in ${city}, and I love ${language}.`;
-console.log(message);
+
+console.log(
+  `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${ process.env.MY_LANGUAGE}.`
+);
+
